74c73840b9ba67281715c8d98eb878b4a090b845
Sascha Holzigel <146719950+Sascha2912@users.noreply.github.com>
2024-01-31 15:41:31 +0100

    Parkplatz-Klasse implementiert.

--- src/Parkhaus/Parkplatz.java
+++ src/Parkhaus/Parkplatz.java
@@ -2,5 +2,24 @@ package Parkhaus;
 
 public class Parkplatz implements IParkhausParkplatz{
 
+    private String id;
+    private boolean frei;
+
+    public Parkplatz(String id){
+        this.frei = true;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+    public boolean istFrei(){
+        return this.frei;
+    }
+    public void belegen(){
+        this.frei = false;
+    }
+    public void freigeben(){
+        this.frei = true;
+    }
 
 }
