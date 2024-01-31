5e7b9432850d0bc443cdc53e3d96c7ac728ce49c
Sascha Holzigel <146719950+Sascha2912@users.noreply.github.com>
2024-01-31 15:31:42 +0100

    Fahrzeug-Klasse implementiert

--- src/Fahrzeuge/Fahrzeug.java
+++ src/Fahrzeuge/Fahrzeug.java
@@ -2,5 +2,24 @@ package Fahrzeuge;
 
 public abstract class Fahrzeug {
 
+    private String kennzeichen;
+
+    public Fahrzeug(String kennzeichen){
+        this.kennzeichen = kennzeichen;
+    }
+
+    public String getKennzeichen(){
+        return this.kennzeichen;
+    }
+    public void setKennzeichen(String kennzeichen){
+        this.kennzeichen = kennzeichen;
+    }
+
+    public void reinfahren(){
+        System.out.println("Das Fahrzeug mit dem Kennzeichen: " + kennzeichen + " fährt ins Parkhaus hinein.");
+    }
+    public void verlassen(){
+        System.out.println("Das Fahrzeug mit dem Kennzeichen: " + kennzeichen + " verlässt das Parkhaus.");
+    }
 
 }
